9a0b242d647283a1bbb2769c7b898baa7184b50c
AbbeyC <abbey.chrystal@gmail.com>
2021-01-19 09:25:25 -0800

    completed dojoSurveyUpdate assignment - had to remove radio buttons and checkboxes to get it to work

--- django/django_fundamentals/dojosurveyUpdate/newSurveyApp/urls.py
+++ django/django_fundamentals/dojosurveyUpdate/newSurveyApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process', views.process)
+    path('survey', views.survey),
+    path('result', views.result)
 ]
--- django/django_fundamentals/dojosurveyUpdate/newSurveyApp/views.py
+++ django/django_fundamentals/dojosurveyUpdate/newSurveyApp/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render, redirect
+LANGS = (
+    'Python',
+    'JavaScript',
+    'C#',
+    'Java'
+)
+LOCATIONS = (
+    'San Jose',
+    'Seattle',
+    'Chicago',
+    'Online'
+)
+STOKES = (
+    'Low',
+    'Medium',
+    'High'
+)
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'form.html')
+    context = {
+        'locations': LOCATIONS,
+        'languages': LANGS,
+        'stokes': STOKES
+    }
+    return render(request, 'form.html',context)
 
-def process(request):
-    if request.method == 'POST':
-        context = {
-            'name': request.POST['name'],
-            'lang': request.POST['language'],
-            'loc': request.POST['location'],
-            'stoke': request.POST['stoke'],
-        }
-        return render(request, 'result.html', context)
-    return render (request, 'result.html')
+def survey(request):
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['result'] = {    
+        'name': request.POST['name'],
+        'lang': request.POST['language'],
+        'loc': request.POST['location'],
+        'stoke': request.POST['stoke'],
+        'comment':request.POST['comment']
+    }
+    return render(request, 'result.html', context)
+    
+def result(request):
+    context = {
+        'result': request.session['result']
+    }    
+    return render(request, 'result.html', context)
