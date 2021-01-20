3f7746a2d4d1ccc3dc2335a6debc1cc0a2954547
AbbeyC <abbey.chrystal@gmail.com>
2021-01-20 12:30:44 -0800

    solution

---
+++ django/django_fundamentals/ninjaGold/findGold/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('reset', views.reset),
+    path('gold', views.process_gold)
+]
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.urls import path
+# from . import views
+	
+# urlpatterns = [
+# 	    path('', views.index),
+# 		path('gold', views.process_gold)
+# 	]
