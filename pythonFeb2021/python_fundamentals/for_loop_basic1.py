673f40947b1d03ef86d187997b319831dc691733
AbbeyC <abbey.chrystal@gmail.com>
2021-02-16 20:14:00 -0800

    update to python fundamentals assignments

---
+++ pythonFeb2021/python_fundamentals/for_loop_basic1.py
@@ -0,0 +1,38 @@
+# Basic - Print all integers of 5 from 5-1000
+for i in range(0, 151, 1):
+    print(i)
+
+# Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+for i in range(5, 1001, 5):
+    print(i)
+
+# Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+for i in range( 1, 101, 1):
+    if i%5 == 0:
+        print("Coding")
+    else:
+        print(i)
+
+# Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+count = 0
+for i in range(1, 500001):
+    if i%2 != 0:
+        count = count + i
+    
+print(count)
+
+# Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+for i in range (2018, -1, -4):
+    print(i)
+
+# Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+def flexCount(lowNum, highNum, mult):
+    for i in range(lowNum, highNum+1):
+        if i%mult ==0:
+            print(i)
+
+flexCount(2,9,3)
+
+
+
