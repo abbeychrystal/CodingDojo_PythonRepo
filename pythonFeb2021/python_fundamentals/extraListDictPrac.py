27a8954a32b81b26ecf5df9081ecb72b343f2fb3
AbbeyC <abbey.chrystal@gmail.com>
2021-02-21 12:43:11 -0800

    finished extra list practice from Zack

--- pythonFeb2021/python_fundamentals/extraListDictPrac.py
+++ pythonFeb2021/python_fundamentals/extraListDictPrac.py
@@ -2,11 +2,12 @@ numbers = [5, [7, 3, 12, 14], [8, 99, 60], 16, [9]]
 
 #Print the following numbers using the above variable:
 
-#1 - 16
-
-#2 - 12
-
-#3 - 9
+# #1 - 16
+# print(numbers[3])
+# #2 - 12
+# print(numbers[1][2])
+# #3 - 9
+# print(numbers[4][0])
 
 
 fido = {"can_roll_over": True, 
@@ -16,10 +17,10 @@ fido = {"can_roll_over": True,
 
 #Print the following things using the above variable:
 
-#4 - False
-
-#5 - "Lois"
-
+# #4 - False
+# print(fido['can_fetch'])
+# #5 - "Lois"
+# print(fido['owners'][1])
 
 superheroes = [
     {"name": "Wonder Woman", "power": "super strength"},
@@ -31,9 +32,11 @@ superheroes = [
 #Print the following things using the above variable:
 
 #6 - "Aquaman"
-
+print(superheroes[2]["name"])
 #7 - "super strength"
-
+print(superheroes[0]['power'])
 #8 - 54
-
+print(superheroes[1]['extra_field'][2])
 #9 - Write a for loop that prints all names in the above variable.
+for superhero in superheroes:
+    print(superhero['name'])
