b549159069d30621cde4f05b60012263e84cd8fc
AbbeyC <abbey.chrystal@gmail.com>
2021-02-24 22:50:46 -0800

    finished DojoSurvey Revisited

---
+++ pythonFeb2021/django_fundamentals/dojosurveyUpdate/newSurveyApp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+LANGS = (
+    'Python',
+    'JavaScript',
+    'C#',
+    'Java'
+)
+LOCATIONS = (
+    'San Jose',
+    'Seattle',
+    'Chicago',
+    'Online'
+)
+STOKES = (
+    'Low',
+    'Medium',
+    'High'
+)
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'locations': LOCATIONS,
+        'languages': LANGS,
+        'stokes': STOKES
+    }
+    return render(request, 'form.html',context)
+
+def survey(request):
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['result'] = {    
+        'name': request.POST['name'],
+        'lang': request.POST['language'],
+        'loc': request.POST['location'],
+        'stoke': request.POST['stoke'],
+        'comment':request.POST['comment']
+    }
+    return redirect("/result") 
+    
+def result(request):
+    context = {
+        'result': request.session['result']
+    }    
+    return render(request, 'result.html', context)
+
+# request.session['name'] = request.POST['name']
